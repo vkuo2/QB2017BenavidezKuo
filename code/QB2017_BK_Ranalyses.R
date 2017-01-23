0c6fe4ae5c16d08f3722a87a76601ff87c4eb553
Venus Kuo <vkuo126@gmail.com>
2017-01-23 15:32:46 -0500

    Adding the BCI census txt and R code to make SbyS matrix

--- code/QB2017_BK_Ranalyses.R
+++ code/QB2017_BK_Ranalyses.R
@@ -14,16 +14,27 @@
 ############################################################
 # Setup work enviroment #
 rm(list = ls())
-setwd("C:/Users/Venus/Github/QB2017BenavidezKuo/")
+setwd("C:/Users/Venus/Github/QB2017BenavidezKuo/data")
 
 # Load packages #
 require("vegan")  #Already contains BCI data
-require("BiodiversityR") #Contains more information on BCI dataset: long/lat/etc
+require("tidyr")
+require(dplyr)
 
-# Load BCI dataset #
-data(BCI) # Only for 1 year it seems 
-dim(BCI)
-View(BCI)
+# Load dataset #
+BCI2010 <- read.delim("BCI2010.txt", header=T)
+BCI1982 <- read.delim("BCI1982.txt", header=T)
+
+# Transform 2010 census data into site by species matrix #
+BCI.2010.SbyS <- group_by(BCI2010, Quadrat) %>% count(Latin) %>% spread(key=Latin, value=n , fill=0)
+
+# Transform 1982 census data # 
+BCI.1982.SbyS <- group_by(BCI1982, Quadrat) %>% count(Latin) %>% spread(key=Latin, value=n , fill=0)
+
+
+
+
+
 
 
 
